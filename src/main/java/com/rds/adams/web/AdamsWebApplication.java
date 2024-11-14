b0459b7fbe4dd424b5b9ae19bbeb6805476995c7
HyunseungJeong <hs.jeong@rnadatasystem.com>
2024-11-14 17:32:47 +0900

    RSF 변경에 따른 수정

--- src/main/java/com/rds/adams/web/AdamsWebApplication.java
+++ src/main/java/com/rds/adams/web/AdamsWebApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication (exclude = SecurityAutoConfiguration.class, scanBasePackages = {"com.rds"})
+@SpringBootApplication (exclude = SecurityAutoConfiguration.class, scanBasePackages = {"com.rds.rsf.core", "com.rds.adams.web"})
 public class AdamsWebApplication {
 
 	public static void main(String[] args) {
