6ff38e14b4b70042c89b379fd6d78d58c93fa05b
junmin park <jm.park@rnadatasystem.com>
2025-02-07 17:20:02 +0900

    redis setting

--- src/main/java/com/rds/adams/web/AdamsWebApplication.java
+++ src/main/java/com/rds/adams/web/AdamsWebApplication.java
@@ -3,7 +3,9 @@ package com.rds.adams.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import  org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession
 @SpringBootApplication (exclude = SecurityAutoConfiguration.class, scanBasePackages = {"com.rds.rsf.core", "com.rds.adams.web"})
 public class AdamsWebApplication {
 
