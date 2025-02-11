49d38250e6f6a9929ee7b69688941f7afcc39086
junmin park <jm.park@rnadatasystem.com>
2025-02-11 16:38:07 +0900

    Modify RedisConfig, application-dev

--- src/main/java/com/rds/adams/web/config/RedisConfig.java
+++ src/main/java/com/rds/adams/web/config/RedisConfig.java
@@ -1,7 +1,6 @@
 package com.rds.adams.web.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,8 +8,10 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@EnableCaching
+
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
 @Configuration
 public class RedisConfig{
 	@Value("${spring.data.redis.host}")
@@ -26,10 +27,10 @@ public class RedisConfig{
 	}
 	
 	@Bean
-	public RedisTemplate<String, String> redisTemplate() {
+	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
 		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
 		
-		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setValueSerializer(new StringRedisSerializer());
 		
