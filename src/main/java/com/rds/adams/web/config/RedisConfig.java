0f997968b32be52ab6528d3298ea44af638a860d
junmin park <jm.park@rnadatasystem.com>
2025-02-11 09:38:43 +0900

    modify RedisConfig

--- src/main/java/com/rds/adams/web/config/RedisConfig.java
+++ src/main/java/com/rds/adams/web/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -20,7 +21,8 @@ public class RedisConfig{
 	
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(redisHost, redisPort);
+		RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
+		return new LettuceConnectionFactory(configuration);
 	}
 	
 	@Bean
